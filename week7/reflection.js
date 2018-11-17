f3974c165110c82510ecff019e3dc6237b585f84
Sajjad Taheri <sajjadt@uci.edu>
2018-11-17 12:07:50 +0000

    more dynamic functions added

--- week7/reflection.js
+++ week7/reflection.js
@@ -1,34 +1,28 @@
-/*globals parse_input:true */
 var fs = require("fs");
 const N = 25;
 
-read_stop_file = function() {
-    var data = fs.readFileSync("../stop_words.txt");
-    data = data.toString();
-    data = data.toLowerCase();
-    data = data.replace(/\n|\r/g, "")
-    return new Set(data.split(","));
-}
-
 if (process.argv.length == 3) {
-    extract_words = 'function(name){data = fs.readFileSync(name);data = data.toString();data = data.replace("/\\n|\\r/g", " ");data = data.toLowerCase();var items = [];var re = /[a-z][a-z]+/g; var matches = []; while (match = re.exec(data)) matches.push(match[0]); for(var entry of matches) {if (!stop_words.has(entry))items.push(entry);}return items;}';
+    scan = 'function(name){data = fs.readFileSync(name);data = data.toString();data = data.replace("/\\n|\\r/g", " ");data = data.toLowerCase();var items = [];var re = /[a-z][a-z]+/g; var matches = []; while (match = re.exec(data)) matches.push(match[0]); return matches;}';
+    remove_stop_words = 'function(words){valid_words = []; var data = fs.readFileSync("../stop_words.txt"); data = data.toString();var stop_words = new Set(data.split(",")); for(var entry of words) {if (!stop_words.has(entry))valid_words.push(entry);}return valid_words;}';
     calc_freq = 'function(word_list){word_freqs = {};for (let word of word_list){if (word in word_freqs) word_freqs[word] += 1; else word_freqs[word] = 1;} return word_freqs;}';
     sort_freq = 'function(word_freqs){var items = Object.entries(word_freqs).map(function(entry) {return [entry[0], entry[1]];});items.sort(function(first, second) {return second[1] - first[1] ;});return items;}';
+    print = 'function(word_freqs){word_freqs.slice(0, N).forEach((item) => {console.log(`${item[0]} - ${item[1]}`);});}';
     file_name = process.argv[2];
 } else {
-    extract_words ='function(_){return [];}';
+    scan ='function(_){return [];}';
+    remove_stop_words = 'function(_){return [];}';
     calc_freq = 'function(_){return {};}';
     sort_freq = 'function(_){return {};}';
+    print = 'function(_){}';
     file_name = '';
 }
 
 // Add functions dynamically to the program
-eval('extract_words = ' + extract_words);
+eval('scan = ' + scan);
+eval('remove_stop_words = ' + remove_stop_words);
 eval('calc_freq = ' + calc_freq);
 eval('sort_freq = ' + sort_freq);
+eval('print = ' + print)
 
-stop_words = read_stop_file();
-var word_freqs = sort_freq(calc_freq(extract_words(file_name)));
-word_freqs.slice(0, N).forEach((item) => {
-    console.log(`${item[0]} - ${item[1]}`);
-});
+print(sort_freq(calc_freq(remove_stop_words(scan(file_name)))));
+
